437421b57fa378926d4665f257a0d82f7c0660ac
chawk89 <32115931+chawk89@users.noreply.github.com>
2022-02-08 17:27:24 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,36 +3,29 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+from transformers import pipeline
+import time
 
 """
-# Welcome to Streamlit!
+# Text Summarizer
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
+Add a snippet of an article to summarize. It may take 5-10 seconds to complete the task! The text summarizer is a transformer model from huggingface.co, pretrained on publisher content. 
 """
 
+article = "Anger and confusion overflowed at the Olympic mixed-team ski jumping final in China after five female competitors were disqualified from the event by officials who said their jumpsuits didn't comply with the rules."
+input = st.text_area("Insert Text", article)
+st.write('Character count: ', len(input))
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+if len(input) > 1500:
+    st.write("Input may be just a bit too long!")
 
-    Point = namedtuple('Point', 'x y')
-    data = []
 
-    points_per_turn = total_points / num_turns
+with st.spinner('Wait for it...'):
+    summarizer = pipeline('summarization')
+    output = summarizer(input, min_length = 20,  max_length = 120, do_sample=False)[0]['summary_text']
+st.success('Done!')
+    
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+st.header('Summary:')
+st.subheader(output)
+st.balloons()
